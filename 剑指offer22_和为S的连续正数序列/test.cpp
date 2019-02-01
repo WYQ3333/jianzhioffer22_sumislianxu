2d0432f7aec8f7491a21cdfb63bf4ceb1134e97c
浅忆梦微凉 <3538922590@qq.com>
2019-02-01 22:36:56 +0800

    大神的代码

--- 剑指offer22_和为S的连续正数序列/test.cpp
+++ 剑指offer22_和为S的连续正数序列/test.cpp
@@ -56,10 +56,42 @@ public:
 	}
 };
 
+//左神的思路，双指针问题
+//当总和小于sum，大指针继续+
+//否则小指针+
+class Solution3 {
+public:
+	vector<vector<int> > FindContinuousSequence(int sum) {
+		vector<vector<int> > allRes;
+		int phigh = 2, plow = 1;
+
+		while (phigh > plow){
+			//计算出了滑动的窗口的大小
+			//左边是滑动窗口的宽度的二倍，右边是滑动窗口的长度
+			int cur = (phigh + plow) * (phigh - plow + 1) / 2;
+			if (cur < sum)
+				phigh++;
+
+			if (cur == sum){
+				vector<int> res;
+				for (int i = plow; i <= phigh; i++)
+					res.push_back(i);
+				allRes.push_back(res);
+				plow++;
+			}
+
+			if (cur > sum)
+				plow++;
+		}
+
+		return allRes;
+	}
+};
+
 void TestFunc(){
-	Solution1 s;
+	Solution s;
 	vector<vector<int>> solution;
-	solution = s.FindContinuousSequence(100);
+	solution = s.FindContinuousSequence(90);
 	if (solution.empty()){
 		cout << "不存在这样的数组" << endl;
 	}
